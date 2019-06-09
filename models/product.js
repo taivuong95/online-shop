71bca614fb6be8dd9d577c0517bc07088c7c1eaa
TaiVuong <taivt95@gmail.com>
2019-06-09 16:03:20 +0700

    Storing and Fetching data via Model

--- models/product.js
+++ models/product.js
@@ -1,14 +1,41 @@
+const fs = require("fs");
+const path = require("path");
+
 const products = [];
 
-module.exports = class Product{
-    constructor(title){
-        this.title = title;
-    }
+module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
 
-    save(){
-        products.push(this);
-    }
-    static fetchAll(){
-        return products;
-    }
-}
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+          console.log(err)
+      })
+    });
+  }
+  static fetchAll(cb) {
+    const p = path.join(
+        path.dirname(process.mainModule.filename),
+        "data",
+        "products.json"
+      );
+      fs.readFile(p, (err, fileContent) => {
+          if(err){
+              cb([])
+          }
+          return cb(JSON.parse(fileContent));
+      })
+  }
+};
