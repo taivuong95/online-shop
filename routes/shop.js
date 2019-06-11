fe679fc55a715a6cef6eec9b7bca7d69f2091e4e
TaiVuong <taivt95@gmail.com>
2019-06-11 20:32:42 +0700

    adding productId and Extracting dynamic params

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,10 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+
 router.get('/cart', shopController.getCart);
+
 router.get('/orders', shopController.getOrders);
 
 router.get('/checkout', shopController.getCheckout);
