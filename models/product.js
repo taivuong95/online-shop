4f9fc2b489cd25232dbe9c04752dabde85c4e4f0
Tai Vuong <tai.vuong@s3corp.com.vn>
2019-06-12 13:58:37 +0700

    update delete Product Data

--- models/product.js
+++ models/product.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
 
+const Cart = require('./cart')
+
 const p = path.join(
   path.dirname(process.mainModule.filename),
   "data",
@@ -49,10 +51,11 @@ module.exports = class Product {
 
   static deleteById(id){
     getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
       const updateProducts = products.filter(prod => prod.id !== id);
       fs.writeFile(p, JSON.stringify(updateProducts), err => {
         if(!err){
-
+            Cart.deleteProduct(id, product.price)
         }
       });
     });
