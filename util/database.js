98ccf55a8b5de6cce4f342291fe2cd83af05e609
TaiVuong <taivt95@gmail.com>
2019-06-15 11:04:44 +0700

    Adding Database Connection and Creating Products and Fetching All Products

--- util/database.js
+++ util/database.js
@@ -1,17 +1,29 @@
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
-const mongoConnect = (callback) => {
+let _db;
+
+const mongoConnect = callback => {
   MongoClient.connect(
-    "mongodb+srv://taivuong:cdhXgwpU@cluster0-bwjk3.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://taivuong:cdhXgwpU@cluster0-bwjk3.mongodb.net/shop?retryWrites=true&w=majority"
   )
     .then(client => {
-      console.log("Connected");
-      callback(client);
+      console.log('Connected!');
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
