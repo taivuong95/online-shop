c232f52b8277227641d56d8483ab3c1ae255372e
TaiVuong <taivt95@gmail.com>
2019-06-16 14:06:33 +0700

    Connecting to the MongoDB Server with Mongoose and Create the Product Schema

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('5d05ab8d5a0e811af8b37f65')
+  User.findById("5d05ab8d5a0e811af8b37f65")
     .then(user => {
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
@@ -27,11 +27,19 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+// mongoConnect(() => {
+//   app.listen(3000);
+// });
+
+mongoose.connect(
+  "mongodb+srv://taivuong:cdhXgwpU@cluster0-bwjk3.mongodb.net/shop?retryWrites=true&w=majority"
+).then(result => {
   app.listen(3000);
+}).catch(err => {
+  console.log(err);
 });
--- util/database.js
+++ util/database.js
@@ -1,29 +1,29 @@
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require('mongodb');
+// const MongoClient = mongodb.MongoClient;
 
-let _db;
+// let _db;
 
-const mongoConnect = callback => {
-  MongoClient.connect(
-    "mongodb+srv://taivuong:cdhXgwpU@cluster0-bwjk3.mongodb.net/shop?retryWrites=true&w=majority"
-  )
-    .then(client => {
-      console.log('Connected!');
-      _db = client.db();
-      callback();
-    })
-    .catch(err => {
-      console.log(err);
-      throw err;
-    });
-};
+// const mongoConnect = callback => {
+//   MongoClient.connect(
+//     "mongodb+srv://taivuong:cdhXgwpU@cluster0-bwjk3.mongodb.net/shop?retryWrites=true&w=majority"
+//   )
+//     .then(client => {
+//       console.log('Connected!');
+//       _db = client.db();
+//       callback();
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       throw err;
+//     });
+// };
 
-const getDb = () => {
-  if (_db) {
-    return _db;
-  }
-  throw 'No database found!';
-};
+// const getDb = () => {
+//   if (_db) {
+//     return _db;
+//   }
+//   throw 'No database found!';
+// };
 
-exports.mongoConnect = mongoConnect;
-exports.getDb = getDb;
+// exports.mongoConnect = mongoConnect;
+// exports.getDb = getDb;
