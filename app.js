5f3ec7569d3950261fd67d49c5d5dbf531e4ea1f
TaiVuong <taivt95@gmail.com>
2019-06-23 12:05:07 +0700

    making add to cart work again

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  User.findById("5d066fbfe6714b013cf2c319")
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
