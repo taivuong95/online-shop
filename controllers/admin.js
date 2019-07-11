fb59089dc74f58796c9f876745593211ae64901f
TaiVuong <taivt95@gmail.com>
2019-07-11 20:53:34 +0700

    Returning Error Pages

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const {validationResult} = require('express-validator/check');
 
 const Product = require('../models/product');
@@ -23,7 +25,7 @@ exports.postAddProduct = (req, res, next) => {
   if(!errors.isEmpty()){
     return res.status(422).render('admin/edit-product', {
       pageTitle: 'Add Product',
-      path: '/admin/edit-product',
+      path: '/admin/add-product',
       editing: false,
       hasError:true,
       product: {
@@ -38,6 +40,7 @@ exports.postAddProduct = (req, res, next) => {
   }
 
   const product = new Product({
+    _id: mongoose.Types.ObjectId('5d06740457716003681a1ddc'),
     title: title,
     price: price,
     description: description,
@@ -52,7 +55,22 @@ exports.postAddProduct = (req, res, next) => {
       res.redirect('/admin/products');
     })
     .catch(err => {
-      console.log(err);
+
+      // return res.status(500).render('admin/edit-product', {
+      //   pageTitle: 'Add Product',
+      //   path: '/admin/add-product',
+      //   editing: false,
+      //   hasError:true,
+      //   product: {
+      //     title:title,
+      //     imageUrl:imageUrl,
+      //     price:price,
+      //     description:description
+      //   },
+      //   errorMessage: 'Database operation failed, please try again.',
+      //   validationErrors: []
+      // });
+      res.redirect('/500')
     });
 };
 
