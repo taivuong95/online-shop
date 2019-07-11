dd3170b268185e5166962a262ebce21f35140ded
TaiVuong <taivt95@gmail.com>
2019-07-11 23:32:15 +0700

    Handling Multipart Form Data

--- routes/admin.js
+++ routes/admin.js
@@ -22,8 +22,7 @@ router.post(
       .isString()
       .isLength({ min: 3 })
       .trim(),
-    body("imageUrl")
-      .isURL(),
+   
     body('price')
     .isFloat(),
     body('description')
